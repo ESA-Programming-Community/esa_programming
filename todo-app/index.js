de0975779c6ac3b7b511eaddd332c30e1e0d002e
destinydinam <destinydinam1@gmail.com>
2024-07-25 17:07:50 +0000

    added the javascript for adding todo to our list

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,1 +1,37 @@
 let todos = [];
+
+const input = document.getElementById("todo-input");
+
+const addBtn = document.querySelector(".add-btn");
+
+const todoList = document.querySelector(".todo-list");
+
+addBtn.addEventListener("click", () => {
+  console.log("button clicked");
+
+  const newTodo = input.value;
+
+  let isDuplicated = false;
+
+  todoList.childNodes.forEach((e) => {
+    const todo = e.firstElementChild.textContent;
+
+    if (todo === newTodo) {
+      isDuplicated = true;
+      return;
+    }
+  });
+
+  if (isDuplicated) {
+    alert("Todo already exists");
+  } else {
+    const newLi = document.createElement("li");
+
+    newLi.innerHTML = `
+    <p>${newTodo}</p>
+    <button class="delete-btn">Delete</button>
+    `;
+
+    todoList.appendChild(newLi);
+  }
+});
